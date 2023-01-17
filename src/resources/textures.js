83a8772ff350fa1ebe4561fcab05a9ad17a66c55
YeSuX <351490949@qq.com>
2023-01-17 20:14:11 +0800

    test: path

--- src/resources/textures.js
+++ src/resources/textures.js
@@ -1,41 +1,41 @@
 //billboardTextures
 let billboardTextures = {};
-billboardTextures.terpSolutionsTexture = './src/jsm/terpSolutions.png';
+billboardTextures.terpSolutionsTexture = '/3d-template/src/jsm/terpSolutions.png';
 billboardTextures.bagHolderBetsTexture =
-  './src/jsm/Bagholdersbetsbillboard.png';
-billboardTextures.homeSweetHomeTexture = './src/jsm/tech-show.jpg';
-billboardTextures.codeDojo = './src/jsm/code-dojo.jpg';
+  '/3d-template/src/jsm/Bagholdersbetsbillboard.png';
+billboardTextures.homeSweetHomeTexture = '/3d-template/src/jsm/tech-show.jpg';
+billboardTextures.codeDojo = '/3d-template/src/jsm/code-dojo.jpg';
 
 //box textures
 let boxTexture = {};
-boxTexture.Github = './src/jsm/githubLogo.png';
-boxTexture.twitter = './src/jsm/twitter.png';
-boxTexture.LinkedIn = './src/jsm/linkedInLogo.png';
-boxTexture.mail = './src/jsm/envelope.png';
-boxTexture.globe = './src/jsm/thunder.png';
-boxTexture.reactIcon = './src/jsm/react.png';
-boxTexture.allSkills = './src/jsm/allSkills.png';
-boxTexture.lensFlareMain = './src/jsm/lensflare0.png';
-boxTexture.skrillex = './src/jsm/skrillex.png';
-boxTexture.edmText = './src/jsm/EDM.png';
-boxTexture.writing = './src/jsm/writing.png';
+boxTexture.Github = '/3d-template/src/jsm/githubLogo.png';
+boxTexture.twitter = '/3d-template/src/jsm/twitter.png';
+boxTexture.LinkedIn = '/3d-template/src/jsm/linkedInLogo.png';
+boxTexture.mail = '/3d-template/src/jsm/envelope.png';
+boxTexture.globe = '/3d-template/src/jsm/thunder.png';
+boxTexture.reactIcon = '/3d-template/src/jsm/react.png';
+boxTexture.allSkills = '/3d-template/src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '/3d-template/src/jsm/lensflare0.png';
+boxTexture.skrillex = '/3d-template/src/jsm/skrillex.png';
+boxTexture.edmText = '/3d-template/src/jsm/EDM.png';
+boxTexture.writing = '/3d-template/src/jsm/writing.png';
 
 //material textures
-let stoneTexture = './src/jsm/stone.png';
-let woodTexture = './src/jsm/woodTexture.jpg';
+let stoneTexture = '/3d-template/src/jsm/stone.png';
+let woodTexture = '/3d-template/src/jsm/woodTexture.jpg';
 
 //text
 let inputText = {};
-inputText.terpSolutionsText = './src/jsm/code-dojo-text.png';
-inputText.activities = './src/jsm/team-members.png';
-inputText.codeDojoText = './src/jsm/code-dojo-text.png';
-inputText.bagholderBetsText = './src/jsm/code-dojo-text.png';
-inputText.homeSweetHomeText = './src/jsm/tech-show-text.png';
-inputText.staticPortfolio = './src/jsm/static-portfolio.png';
+inputText.terpSolutionsText = '/3d-template/src/jsm/code-dojo-text.png';
+inputText.activities = '/3d-template/src/jsm/team-members.png';
+inputText.codeDojoText = '/3d-template/src/jsm/code-dojo-text.png';
+inputText.bagholderBetsText = '/3d-template/src/jsm/code-dojo-text.png';
+inputText.homeSweetHomeText = '/3d-template/src/jsm/tech-show-text.png';
+inputText.staticPortfolio = '/3d-template/src/jsm/static-portfolio.png';
 
 //SVG
 let SVG = {};
-SVG.reactLogo = './src/jsm/react-svg.svg';
+SVG.reactLogo = '/3d-template/src/jsm/react-svg.svg';
 
 //URLs
 let URL = {};
